3ccf4e2380cecf752bcc86019cd1d6bdecfc1e45
Alex Seeley <alexseeley134@gmail.com>
2023-12-02 22:02:14 -0700

    Create several functions to add book to library
    I created a template in the html for the book's card. Then, I created
    several functions to contribute Title, Author, Pages, and Read values
    to the DOM. Then, I used these functions in a larger
    addBookToLibrary function. I used this function to add the template
    pre-existing books for the library.

--- script.js
+++ script.js
@@ -7,6 +7,49 @@ function Book(title, author, pages, read) {
     (this.read = Boolean(read));
 }
 
+function addTitle(bookCard, object) {
+  let bookTitle = document.createElement("h1");
+  bookTitle.classList.add("title");
+  bookCard.appendChild(bookTitle);
+  bookTitle.textContent = object.title;
+}
+
+function addAuthor(bookCard, object) {
+  let bookAuthor = document.createElement("h2");
+  bookAuthor.classList.add("author");
+  bookCard.appendChild(bookAuthor);
+  bookAuthor.textContent = object.author;
+}
+
+function addPages(bookCard, object) {
+  let bookPages = document.createElement("div");
+  bookPages.classList.add("page-number");
+  bookCard.appendChild(bookPages);
+  bookPages.textContent = object.pages;
+}
+
+function addRead(bookCard, object) {
+  let bookRead = document.createElement("div");
+  bookRead.classList.add("read");
+  bookCard.appendChild(bookRead);
+  if (object.read == true) {
+    bookRead.textContent = "This book has been read";
+  } else {
+    bookRead.textContent = "This book has not been read";
+  }
+}
+
+function addBookToLibrary(object) {
+  let bookCard = document.createElement("div");
+  bookCard.classList.add("card");
+  libraryArea.appendChild(bookCard);
+
+  addTitle(bookCard, object);
+  addAuthor(bookCard, object);
+  addPages(bookCard, object);
+  addRead(bookCard, object);
+}
+
 const percyJackson = new Book(
   "Percy Jackson & the Olympians: The Lightning Thief",
   "Pierce Brown",
@@ -18,6 +61,8 @@ const redRising = new Book("Red Rising", "Richard Russell Riordan", 377, false);
 
 const eragon = new Book("Eragon", "Christopher Paolini", 544, true);
 
-function addBookToLibrary() {
-  // do stuff here
-}
+const libraryArea = document.querySelector("#library-container");
+
+addBookToLibrary(percyJackson);
+addBookToLibrary(redRising);
+addBookToLibrary(eragon);
